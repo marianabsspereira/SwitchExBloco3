a60d731da590c033fda72149c9e14c3d8086f9bf
Mariana Pereira <marianabsspereira@gmail.com>
2021-10-28 15:26:22 +0100

    Ex9 com validacao dados user e ex11 com sol alternativa

--- src/main/java/Bloco3Ex11.java
+++ src/main/java/Bloco3Ex11.java
@@ -20,6 +20,18 @@ public class Bloco3Ex11 {
         }
         conta = conta / 2;
         System.out.println("Nº de formas possíveis: " + conta);
+
+        // solução alternativa (só os ciclos), só conta metade da matriz
+        // Neste contexto é igual à anterior, poderia ser melhor se fosse para guardar as soluções
+        // Em vez de apenas retornar o seu nº
+        /*for (int i = 0; i <= 10; i++){
+            for (int j = 0; j <= 10; j++) {
+                if (i + j == N && i >= j){
+                    conta ++;
+                }
+            }
+        }
+        System.out.println("Nº de formas possíveis: " + conta);*/
     }
 
 }
--- src/main/java/Bloco3Ex9.java
+++ src/main/java/Bloco3Ex9.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Bloco3Ex9 { //falta validar dados do utilizador
+public class Bloco3Ex9 { //Já com validação dos 2 dados inseridos pelo utilizador
     public static void main(String[] args){
         double salarioBase;
         double horasExtra = 0;
@@ -15,9 +15,27 @@ public class Bloco3Ex9 { //falta validar dados do utilizador
             System.out.println("Inserir salário base:");
             salarioBase = sc.nextDouble();
             sc.nextLine();
+
+            if (salarioBase <= 0){
+                do {
+                    System.out.println("Salário base inválido, inserir novo valor:");
+                    salarioBase = sc.nextDouble();
+                    sc.nextLine();
+                } while (salarioBase <= 0);
+            }
+
             System.out.println("Inserir horas extra:");
             horasExtra = sc.nextDouble();
             sc.nextLine();
+
+            if (horasExtra < 0 && horasExtra != -1){
+                do {
+                    System.out.println("Horas extra inválidas, inserir novo valor:");
+                    horasExtra = sc.nextDouble();
+                    sc.nextLine();
+                } while (horasExtra < 0 && horasExtra != -1);
+            }
+
             salarioMensal = salarioBase + salarioBase * PERCENTAGEMHE * horasExtra;
             if (horasExtra != -1){
                 System.out.println("Salário mensal: " + salarioMensal);
@@ -26,6 +44,8 @@ public class Bloco3Ex9 { //falta validar dados do utilizador
             somaSalarios += salarioMensal;
         } while (horasExtra != -1);
 
+        // 2 linhas para excluir o salário base e horas extra inseridos
+        // no ultimo ciclo quando horas extra = -1 que termina o programa
         somaSalarios = somaSalarios - salarioBase - salarioBase * PERCENTAGEMHE * horasExtra;
         contaFuncionarios --;
         System.out.println("Média salários mês: " + (somaSalarios/contaFuncionarios));
